48eb6932a4965f7c2e0dd7b3eaf1575d9be30923
Davidmitseruk <davidmitseruk@gmail.com>
2025-09-14 20:27:02 +0200

    lol

--- students-localhost/src/studentsForm.js
+++ students-localhost/src/studentsForm.js
@@ -1,115 +1,138 @@
-import React, { Component } from "react";
+import { Component } from "react";
 import "./StudentForm.css";
 
-class StudentForm extends Component{
-     constructor(props){
-        super(props)
-        this.state = {
-            name: "",
-            group: "",
-            course: "",
-            present: false,
-            students: [],
-        };
-     }
-     handleChange = (e) => {
-        const { name, value, type, checked } = e.target;
+class StudentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      group: "",
+      course: "",
+      present: false,
+      students: [],
+    };
+  }
 
-        this.setState({
-        [name]: type === "checkbox" ? checked : value,
-            });
-        };
+  componentDidMount() {
+    const savedStudents = localStorage.getItem("students");
+    if (savedStudents) {
+      this.setState({ students: JSON.parse(savedStudents) });
+    }
+  }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.students !== this.state.students) {
+      localStorage.setItem("students", JSON.stringify(this.state.students));
+    }
+  }
 
-    handleSubmit = (e) => {
-        e.preventDefault();
+  handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    this.setState({
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
 
-        const { name, group, course, present } = this.state;
-        const newStudent = { name, group, course, present };
+  addStudent = (e) => {
+    e.preventDefault();
+    const { name, group, course, present, students } = this.state;
 
-        this.setState((prevState) => ({
-            students: [...prevState.students, newStudent],
-            name: "", 
-            group: "",
-            course: "",
-            present: false,
-        }));
-        };
+    if (name.trim() !== "" && group.trim() !== "" && course.trim() !== "") {
+      const newStudent = { name, group, course, present };
+      this.setState({
+        students: [...students, newStudent],
+        name: "",
+        group: "",
+        course: "",
+        present: false,
+      });
+    }
+  };
 
-    
-        render() {
-  return (
-    <div className="container">
-      <div className="form-container">
-        <h2>Student Form</h2>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="name"
-              value={this.state.name}
-              onChange={this.handleChange}
-              required
-            />
-          </label>
+  deleteStudent = (index) => {
+    const updatedStudents = this.state.students.filter((_, i) => i !== index);
+    this.setState({ students: updatedStudents });
+  };
 
-          <label>
-            Group:
-            <input
-              type="text"
-              name="group"
-              value={this.state.group}
-              onChange={this.handleChange}
-              required
-            />
-          </label>
+  render() {
+    return (
+      <div className="container">
+        <div className="form-container">
+          <h2>Student Form</h2>
+          <form onSubmit={this.addStudent}>
+            <label>
+              Name:
+              <input
+                type="text"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleChange}
+                required
+              />
+            </label>
 
-          <div>
-            Course:
-            {["HTML", "CSS", "JS", "React"].map((c) => (
-              <label key={c} style={{ marginLeft: 8 }}>
-                <input
-                  type="radio"
-                  name="course"
-                  value={c}
-                  checked={this.state.course === c}
-                  onChange={this.handleChange}
-                  required
-                />
-                {c}
-              </label>
+            <label>
+              Group:
+              <input
+                type="text"
+                name="group"
+                value={this.state.group}
+                onChange={this.handleChange}
+                required
+              />
+            </label>
+
+            <div>
+              Course:
+              {["HTML", "CSS", "JS", "React"].map((c) => (
+                <label key={c} style={{ marginLeft: 8 }}>
+                  <input
+                    type="radio"
+                    name="course"
+                    value={c}
+                    checked={this.state.course === c}
+                    onChange={this.handleChange}
+                    required
+                  />
+                  {c}
+                </label>
+              ))}
+            </div>
+
+            <label>
+              <input
+                type="checkbox"
+                name="present"
+                checked={this.state.present}
+                onChange={this.handleChange}
+              />
+              Present
+            </label>
+
+            <button type="submit">Add Student</button>
+          </form>
+        </div>
+
+        <div className="list-container">
+          <h3>Students:</h3>
+          <ul>
+            {this.state.students.map((student, index) => (
+              <li key={index}>
+                {student.name} - {student.group} - {student.course} -{" "}
+                {student.present ? "Present" : "Absent"}
+                <button
+                  style={{ marginLeft: 10 }}
+                  onClick={() => this.deleteStudent(index)}
+                >
+                  Delete
+                </button>
+              </li>
             ))}
-          </div>
-
-          <label>
-            <input
-              type="checkbox"
-              name="present"
-              checked={this.state.present}
-              onChange={this.handleChange}
-            />
-            Present
-          </label>
-
-          <button type="submit">Add Student</button>
-        </form>
+          </ul>
+        </div>
       </div>
+    );
+  }
+}
 
-      <div className="list-container">
-        <h3>Students:</h3>
-        <ul>
-          {this.state.students.map((student, index) => (
-            <li key={index}>
-              {student.name} - {student.group} - {student.course} -{" "}
-              {student.present ? "Present" : "Absent"}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-}}
-
-
-export default StudentForm
+export default StudentForm;
